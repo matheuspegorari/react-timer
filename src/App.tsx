74edb62b5e8abf0aa4c0f361121ee64816bf16f8
Matheus Pegorari <pegorari42@gmail.com>
2024-06-15 11:12:30 -0300

    eslint: configuration and fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { Button } from './components/Button';
-import { ThemeProvider } from 'styled-components';
-import { defaultTheme } from './styles/themes/default';
-import { GlobalStyle } from './styles/globals';
+import { ThemeProvider } from 'styled-components'
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/globals'
+import { defaultTheme } from './styles/themes/default'
 
 function App() {
-    return (
-        <ThemeProvider theme={defaultTheme} >
-            <Button variant="primary" />
-            <Button variant="secondary" />
-            <Button variant="danger" />
-            <Button variant="success" />
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
 
-            <GlobalStyle/>
-        </ThemeProvider>
-    );
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
     * {
@@ -9,12 +9,12 @@ export const GlobalStyle = createGlobalStyle`
 
     :focus {
         outline: 0;
-        box-shadow: 0 0 0 2px ${p => p.theme["green-500"]};
+        box-shadow: 0 0 0 2px ${(p) => p.theme['green-500']};
     }
 
     body {
-        background-color: ${p => p.theme["gray-900"]};
-        color: ${p => p.theme["gray-300"]}
+        background-color: ${(p) => p.theme['gray-900']};
+        color: ${(p) => p.theme['gray-300']}
 
     }
 
